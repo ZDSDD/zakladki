0ace3714035b39da36068382ccf4eabf3f04bb67
Sebastian Sadowy <sebasadowy@gmail.com>
2024-09-28 13:37:54 +0200

    add script to setup project

---
+++ README.md
@@ -0,0 +1,8 @@
+### env
+
+```
+PORT: "8080"
+DB_URL:"postgres://<user>:<password>@<host>:<port>/<dbname>?sslmode=disable"
+JWT_SECRET=<long random string> // openssl rand -base64 64
+PLATFORM= <"dev" for admin access>
+```
---
+++ setup_postgres_container.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Docker run command to start PostgreSQL container
+docker run --name zakladki_postgres \
+-e POSTGRES_USER=seba \
+-e POSTGRES_PASSWORD=postgres \
+-e POSTGRES_DB=zakladki \
+-v /opt/postgres/data:/var/lib/postgresql/data \
+-p 5432:5432 \
+-d postgres
+
